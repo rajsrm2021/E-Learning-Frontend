f93de1f733cb8035d1981e90d07e4681532a987d
rajsrm2021 <rj9769@srmist.edu.in>
2023-09-19 03:20:31 +0530

    razorpay integration done

--- src/components/Payments/Subscribe.jsx
+++ src/components/Payments/Subscribe.jsx
@@ -6,9 +6,76 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { server } from '../../redux/store';
+import { buySubscription } from '../../redux/actions/user';
+import toast from 'react-hot-toast';
+import logo from "../../assets/images/logo.png"
 
-const Subscribe = () => {
+const Subscribe = ({user}) => {
+  const dispatch = useDispatch();
+  const [key, setKey] = useState('');
+
+  const { loading, error, subscriptionId } = useSelector(
+    state => state.subscription
+  );
+
+  const subscribeHandler = async () => {
+    const { data } = await axios.get(`${server}/razorpaykey`);
+
+    setKey(data.key);
+
+    dispatch(buySubscription());
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+    // if (courseError) {
+    //   toast.error(courseError);
+    //   dispatch({ type: 'clearError' });
+    // }
+    if (subscriptionId) {
+      const openPopUp = () => {
+        const options = {
+          key,
+          name: 'SRM Video Hub',
+          description: 'Get access to all premium content',
+          image: logo,
+          subscription_id: subscriptionId,
+          callback_url: `${server}/paymentverification`,
+          prefill: {
+            name: user.name,
+            email: user.email,
+            contact: '',
+          },
+          notes: {
+            address: 'Raj kumar jaiswal mullai D block SRMIST.',
+          },
+          theme: {
+            color: '#FFC800',
+          },
+        };
+
+        const razor = new window.Razorpay(options);
+        razor.open();
+      };
+      openPopUp();
+    }
+  }, [
+    dispatch,
+    error,
+    // courseError,
+    user.name,
+    user.email,
+    key,
+    subscriptionId,
+  ]);
+
   return (
     <Container h={'90vh'} p={'16'}>
       <Heading children="Welcome" my={'8'} textAlign={'center'} />
@@ -29,7 +96,13 @@ const Subscribe = () => {
             <Heading size={'md'} children={'$299/- only'} />
           </VStack>
 
-          <Button my={'8'} w={'full'} colorScheme="yellow">
+          <Button
+            my={'8'}
+            w={'full'}
+            colorScheme="yellow"
+            onClick={subscribeHandler}
+            isLoading={loading}
+          > 
             Buy Now
           </Button>
         </Box>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { profileReducer, userReducer } from './reducers/userReducer';
+import { profileReducer, subscriptionReducer, userReducer } from './reducers/userReducer';
 import { courseReducer } from './reducers/courseReducer';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     profile: profileReducer,
-    course:courseReducer
+    course:courseReducer,
+    subscription:subscriptionReducer
   },
 });
 
