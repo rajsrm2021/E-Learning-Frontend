da41c544ccfabcaff0a900ced2e10ba54a674fa9
rajsrm2021 <rj9769@srmist.edu.in>
2023-09-20 18:37:53 +0530

    admin users route backend integrated

--- src/components/Admin/Users/Users.jsx
+++ src/components/Admin/Users/Users.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Box,
   Button,
@@ -17,26 +17,50 @@ import {
 import { RiDeleteBin7Fill } from 'react-icons/ri';
 import cursorImage from '../../../assets/images/cursor.png';
 import Sidebar from '../Sidebar';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteUser, getAllUsers, updateUserRole } from '../../../redux/actions/admin';
+import Loader from '../../layout/Loader/Loader';
+import {toast} from "react-hot-toast";
 
 const Users = () => {
-  const users = [
-    {
-      name: 'raj jaiswal',
-      email: 'raj@gmail.com',
-      subscription: {
-        status: 'active',
-      },
-      _id: 'kduhgfiuaghioau',
-      role: 'Admin',
-    },
-  ];
+  const dispatch = useDispatch();
+  // const users = [
+  //   {
+  //     name: 'raj jaiswal',
+  //     email: 'raj@gmail.com',
+  //     subscription: {
+  //       status: 'active',
+  //     },
+  //     _id: 'kduhgfiuaghioau',
+  //     role: 'Admin',
+  //   },
+  // ];
+
+  const { users, loading, error, message } = useSelector(state => state.admin);
+  console.log(users);
 
   const updateHandler = userId => {
-    console.log(userId);
+    // console.log(userId);
+    dispatch(updateUserRole(userId));
   };
   const deleteButtonHandler = userId => {
-    console.log(userId);
+    // console.log(userId);
+    dispatch(deleteUser(userId));
   };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+
+    if (message) {
+      toast.success(message);
+      dispatch({ type: 'clearMessage' });
+    }
+    dispatch(getAllUsers());
+  }, [dispatch,error,message]);
+
   return (
     <Grid
       css={{
@@ -45,40 +69,45 @@ const Users = () => {
       minH={'100vh'}
       templateColumns={['1fr', '5fr 1fr']}
     >
-      <Box p={['0', '16']} overflowX={'auto'}>
-        <Heading
-          textTransform={'uppercase'}
-          children="All users"
-          my={'16'}
-          textAlign={['center', 'left']}
-        />
+      {loading ? (
+        <Loader color="purple.500" />
+      ) : (
+        <Box p={['0', '16']} overflowX={'auto'}>
+          <Heading
+            textTransform={'uppercase'}
+            children="All users"
+            my={'16'}
+            textAlign={['center', 'left']}
+          />
 
-        <TableContainer w={['100vw', 'full']}>
-          <Table variant={'simple'}>
-            <TableCaption>All available users in the database</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>Id</Th>
-                <Th>Name</Th>
-                <Th>Email</Th>
-                <Th>Role</Th>
-                <Th>Subscription</Th>
-                <Th isNumeric>Action</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {users.map(item => (
-                <Row
-                  key={item._id}
-                  item={item}
-                  updateHandler={updateHandler}
-                  deleteButtonHandler={deleteButtonHandler}
-                />
-              ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
+          <TableContainer w={['100vw', 'full']}>
+            <Table variant={'simple'}>
+              <TableCaption>All available users in the database</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Id</Th>
+                  <Th>Name</Th>
+                  <Th>Email</Th>
+                  <Th>Role</Th>
+                  <Th>Subscription</Th>
+                  <Th isNumeric>Action</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {users &&
+                  users.map(item => (
+                    <Row
+                      updateHandler={updateHandler}
+                      deleteButtonHandler={deleteButtonHandler}
+                      key={item._id}
+                      item={item}
+                    />
+                  ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      )}
 
       <Sidebar />
     </Grid>
@@ -90,11 +119,16 @@ export default Users;
 function Row({ item, updateHandler, deleteButtonHandler }) {
   return (
     <Tr>
-      <Td>{item._id}</Td>
+      <Td>#{item._id}</Td>
       <Td>{item.name}</Td>
       <Td>{item.email}</Td>
       <Td>{item.role}</Td>
-      <Td>{item.subscription.status === 'active' ? 'Active' : 'Not Active'}</Td>
+      <Td>
+        {item.subscription && item.subscription.status === 'active'
+          ? 'Active'
+          : 'Not Active'}
+      </Td>
+
       <Td isNumeric>
         <HStack justifyContent={'flex-end'}>
           <Button
