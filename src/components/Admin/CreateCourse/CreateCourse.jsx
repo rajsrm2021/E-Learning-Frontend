95f435c8f1f15bd88109fdb36b9c5e181d0862d6
rajsrm2021 <rj9769@srmist.edu.in>
2023-09-20 14:56:26 +0530

    create course backend integrated

--- src/components/Admin/CreateCourse/CreateCourse.jsx
+++ src/components/Admin/CreateCourse/CreateCourse.jsx
@@ -1,5 +1,5 @@
 import {
-    Button,
+  Button,
   Container,
   Grid,
   Heading,
@@ -8,10 +8,15 @@ import {
   Select,
   VStack,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import cursorImage from '../../../assets/images/cursor.png';
-import Sidebar from '../Sidebar';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import cursor from '../../../assets/images/cursor.png';
+import { createCourse } from '../../../redux/actions/admin';
 import { fileUploadCss } from '../../Auth/Register';
+import Sidebar from '../Sidebar';
+import toast from 'react-hot-toast';
 
 const CreateCourse = () => {
   const [title, setTitle] = useState('');
@@ -21,17 +26,20 @@ const CreateCourse = () => {
   const [image, setImage] = useState('');
   const [imagePrev, setImagePrev] = useState('');
 
+  const dispatch = useDispatch();
+  const { loading, error, message } = useSelector(state => state.admin);
+
   const categories = [
-    'Data Structures',
-    'Algorithms',
-    'Advance Calcus',
+    'Web development',
+    'Artificial Intellegence',
+    'Data Structure & Algorithm',
     'App Development',
     'Data Science',
+    'Game Development',
   ];
 
   const changeImageHandler = e => {
     const file = e.target.files[0];
-
     const reader = new FileReader();
 
     reader.readAsDataURL(file);
@@ -42,62 +50,85 @@ const CreateCourse = () => {
     };
   };
 
+  const submitHandler = e => {
+    e.preventDefault();
+    const myForm = new FormData();
+    myForm.append('title', title);
+    myForm.append('description', description);
+    myForm.append('category', category);
+    myForm.append('createdBy', createdBy);
+    myForm.append('file', image);
+    dispatch(createCourse(myForm));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+
+    if (message) {
+      toast.success(message);
+      dispatch({ type: 'clearMessage' });
+    }
+  }, [dispatch, error, message]);
+
   return (
     <Grid
       css={{
-        cursor: `url(${cursorImage}), default`,
+        cursor: `url(${cursor}), default`,
       }}
       minH={'100vh'}
       templateColumns={['1fr', '5fr 1fr']}
     >
-      <Container py={'16'}>
-        <form>
+      <Container py="16">
+        <form onSubmit={submitHandler}>
           <Heading
             textTransform={'uppercase'}
-            children="Creat course"
-            my={'16'}
+            children="Create Course"
+            my="16"
             textAlign={['center', 'left']}
           />
-          <VStack m={'auto'} spacing={'8'}>
+
+          <VStack m="auto" spacing={'8'}>
             <Input
               value={title}
               onChange={e => setTitle(e.target.value)}
-              type="text"
-              placeholder={'Name'}
+              placeholder="Title"
+              type={'text'}
               focusBorderColor="purple.300"
-            />
+            />{' '}
             <Input
               value={description}
               onChange={e => setDescription(e.target.value)}
-              type="text"
-              placeholder={'Description'}
+              placeholder="Description"
+              type={'text'}
               focusBorderColor="purple.300"
             />
             <Input
               value={createdBy}
               onChange={e => setCreatedBy(e.target.value)}
-              type="text"
-              placeholder={'Creator'}
+              placeholder="Creator Name"
+              type={'text'}
               focusBorderColor="purple.300"
             />
-
             <Select
               focusBorderColor="purple.300"
               value={category}
               onChange={e => setCategory(e.target.value)}
             >
-              <option value={''}>Category</option>
+              <option value="">Category</option>
+
               {categories.map(item => (
-                <option value={item} key={item}>
+                <option key={item} value={item}>
                   {item}
                 </option>
               ))}
             </Select>
-
             <Input
               accept="image/*"
               required
-              type="file"
+              type={'file'}
               focusBorderColor="purple.300"
               css={{
                 '&::file-selector-button': {
@@ -107,12 +138,17 @@ const CreateCourse = () => {
               }}
               onChange={changeImageHandler}
             />
-
             {imagePrev && (
-              <Image src={imagePrev} boxSize={'64'} objectFit={'contain'} />
+              <Image src={imagePrev} boxSize="64" objectFit={'contain'} />
             )}
-
-            <Button w={'full'} colorScheme='purple' type='submit'>Create</Button>
+            <Button
+              isLoading={loading}
+              w="full"
+              colorScheme={'purple'}
+              type="submit"
+            >
+              Create
+            </Button>
           </VStack>
         </form>
       </Container>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { profileReducer, subscriptionReducer, userReducer } from './reducers/userReducer';
 import { courseReducer } from './reducers/courseReducer';
+import { adminReducer } from './reducers/adminReducer';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     profile: profileReducer,
     course:courseReducer,
-    subscription:subscriptionReducer
+    subscription:subscriptionReducer,
+    admin: adminReducer,
   },
 });
 
 export default store;
 
-export const server = 'https://server-qv5e.vercel.app/api/v1';
-// export const server = 'http://localhost:4000/api/v1';
+// export const server = 'https://server-qv5e.vercel.app/api/v1';
+export const server = 'http://localhost:4000/api/v1';
