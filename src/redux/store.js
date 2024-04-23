33bf55c100a62d758e9ff4352657caa11e2ebea3
rajsrm2021 <rj9769@srmist.edu.in>
2024-04-23 22:05:34 +0530

    added-fab icon

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { profileReducer, subscriptionReducer, userReducer } from './reducers/userReducer';
+import {
+  profileReducer,
+  subscriptionReducer,
+  userReducer,
+} from './reducers/userReducer';
 import { courseReducer } from './reducers/courseReducer';
 import { adminReducer } from './reducers/adminReducer';
 import { otherReducer } from './reducers/otherReducer';
@@ -8,10 +12,10 @@ const store = configureStore({
   reducer: {
     user: userReducer,
     profile: profileReducer,
-    course:courseReducer,
-    subscription:subscriptionReducer,
+    course: courseReducer,
+    subscription: subscriptionReducer,
     admin: adminReducer,
-    other:otherReducer,
+    other: otherReducer,
   },
 });
 
